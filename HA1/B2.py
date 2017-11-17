797e7f3446dff83828033d7caad2e5c2e795c5ce
antongoransson <anton.goransson95@gmail.com>
2017-11-17 17:29:13 +0100

    cleaned up C1

--- HA1/B2.py
+++ HA1/B2.py
@@ -1,24 +1,17 @@
 import hashlib
-from converter import *
 from random import randrange
 import numpy as np
 
 def micro_mint_sim(u, k, c):
     nbr_coins = attempts = 0
-    attempts += 1
     bins = [0 for x in range(2**u)]
     while nbr_coins < c:
-        x = randrange(0, 2**(2*u))
-        hash_value = hash(int_to_bytes(x), u)
+        x = randrange(2**(2*u))
         bins[hash_value] += 1
         if bins[hash_value] == k:
             nbr_coins += 1
         attempts += 1
     return attempts
-
-def hash(x, u):
-    hash_hex = hashlib.md5(x).hexdigest()
-    return hex_to_int(hash_hex) % 2**u
 
 def attempts_estimate(u = 16, k = 2, c = 1, width = 22):
     lambda_interval = 3.66
--- HA1/C1.py
+++ HA1/C1.py
@@ -1,57 +1,45 @@
 from random import randrange, sample
 from converter import *
-import operator
-import functools
+from functools import reduce
 
 
-def calc_B_i(k, ID, n, e, inv, size = 2**20):
-    B,quadruples, a, c, d, r  = [[0 for i in range(2*k)] for i in range(6)]
-    B_TEST = []
+def calculate_B(k, ID, n, e, inv, size = 2**20):
+    B, quadruples = [[0 for i in range(2*k)] for i in range(2)]
+    f_x_y = []
     for i in range(2 * k):
-        a_i, c_i, d_i, r_i= [randrange(1, size) for x in range(4)]
-        B_i = calcul_B_i(a_i, c_i, d_i, r_i, ID, e, n, B_TEST)
-        B[i] = B_i
-        quadruples[i] = a_i, c_i, d_i ,r_i
-    return B, B_TEST, quadruples
+        a_i, c_i, d_i, r_i = [randrange(1, size) for x in range(4)]
+        B[i] = calculate_B_i(a_i, c_i, d_i, r_i, ID, e, n, f_x_y)
+        quadruples[i] = a_i, c_i, d_i, r_i
+    return B, quadruples, f_x_y
 
-def calcul_B_i(a, c, d, r, ID, e, n, B_TEST = None):
+def calculate_B_i(a, c, d, r, ID, e, n, f = None):
     x_i = sha1_hash(a + c)
     y_i = sha1_hash(a ^ ID + d)
     f_x_y = bytes_to_int(x_i) * bytes_to_int(y_i)
 
-    if B_TEST is not None:
-        B_TEST.append(f_x_y)
+    if f is not None:
+        f.append(f_x_y)
     B_i = r**e * f_x_y % n
-
     return B_i
 
-def receive_signature(B, ID, inv, e, n, k, R, quadruples):
+def receive_signature(B, ID, priv_key, e, n, R, quadruples):
     for i in R:
         a, c, d, r = quadruples[i]
-        B_i = calcul_B_i(a, c, d, r, ID, e, n)
+        B_i = calculate_B_i(a, c, d, r, ID, e, n)
         if(B_i != B[i]):
             raise Exception("Values are not equal")
-    S = 1
-    R_I = [i for i in range(len(B)) if i not in R]
-    # functools.reduce(lambda:, [1,2,3,4,5,6], 1)
-    for i in R_I:
-        S *= pow(B[i], inv, n)
-    S = S % n
-    print("Bank",S)
-    return S, R_I
+    R_sign = [i for i in range(len(B)) if i not in R]
+    B_to_sign = [B[i] for i in R_sign]
+    signed_B = pow(reduce(lambda x, y : x * y %n, B_to_sign), priv_key, n)
+    return signed_B, R_sign
 
-def calc_S(S, quadruples_R_I, n, d, R_I):
-    x = 1
-    for i in R_I:
-        _, _, _, r = quadruples_R_I[i]
-        x *= r
-    x = (S * mulinv(x, n)) % n
-    print("Alice", x)
-    return x
+def calc_S(signed_B, quadruples, n, R_sign):
+    r = [quadruples[i][3] for i in R_sign]
+    S = signed_B * mulinv(reduce(lambda x, y : x * y , r), n) % n
+    return S
 
-def getIndices(B):
-    return sorted(sample(range(len(B)), len(B) // 2))
-
+# Taken from
+#https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
 def extendex_euc_alg(x, n):
     x0, x1, y0, y1 = 1, 0, 0, 1
     while n != 0:
@@ -69,29 +57,24 @@ def mulinv(b, n):
 def totient(p, q):
     return (p - 1) * (q - 1)
 
-p = 1033
-q = 1299721
-e = 17
-n = (p) * (q) # 127*89= 11303
+p, q, e = 1300097, 1299721, 17
+n = p * q # 127*89= 11303
 totient_n = totient(p, q)
-ID =  1234415161711
-k = 4
+ID =  1251261711
+k = 100
 priv_key  = mulinv(e, totient_n)
 # print("INV", priv_key)
-for i in range(10):
-    B, B_TEST, quadruples = calc_B_i(k, ID, n, e , priv_key) # STEP 1
-    R = getIndices(B) # STEP 2
-    quadruples_R = { i: quadruples[i] for i in R}
-    sign, R_I = receive_signature(B, ID, priv_key, e, n, k, R, quadruples_R) #STEP 3.2
-    quadruples_R_I = { i: quadruples[i] for i in R_I}
-    x = 1
-    y = 1
-    for i in R_I:
-        x *= pow(B_TEST[i], priv_key, n)
-    for i in R_I:
-        y *= B_TEST[i]
-    x = x % n
-    S = calc_S(sign, quadruples_R_I, n, priv_key, R_I)
-    print(S == x)
-    print(y % n)
-    print(pow(S, e, n))
+for i in range(1000):
+    B, quadruples, f_x_y = calculate_B(k, ID, n, e, priv_key) # STEP 1
+    R = sample(range(len(B)), len(B) // 2) # STEP 2
+    quadruples_R = {i: quadruples[i] for i in R}
+    signed_B, R_sign = receive_signature(B, ID, priv_key, e, n, R, quadruples_R) #STEP 3.2
+
+    f_sign = [f_x_y[i] for i in R_sign]
+    prod_sum_f = reduce(lambda x, y: x * y, f_sign)
+    x = pow(prod_sum_f, priv_key, n)
+
+    S = calc_S(signed_B, quadruples, n, R_sign)
+    print("SIGNED VALUE OK", S == x)
+    print("SIGNATURE OK", prod_sum_f % n == pow(S, e, n))
+    # print(S)
