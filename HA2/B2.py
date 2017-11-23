de5b3fe02e6eec974725bd7fc8914009f4f6611d
antongoransson <anton.goransson95@gmail.com>
2017-11-23 17:10:46 +0100

    changed to Ip-address module

--- HA2/B2.py
+++ HA2/B2.py
@@ -1,44 +1,40 @@
 from converter import *
 from pcapfile import savefile
+from ipaddress import IPv4Address
+
+def all_is_disjoint(R, R_other):
+    return all([R_i.isdisjoint(R_other) for R_i in R])
 
 def learning_phase(sender_ip, mix_ip, m, file_in):
     ip_sources, ip_dests = parse_pcap_file(file_in)
-    R, sets, i = [], [], 0
+    R, R_other, i = [], [], 0
     while i is not None:
         i = ip_sources.index(sender_ip, i)
         start = i = ip_sources.index(mix_ip, i)
         try: i = ip_dests.index(mix_ip, i)
         except: i = None
         receivers = ip_dests[start:i]
-        t_set = R if len(R) < m and is_disjoint(R, receivers) else sets
-        t_set.append(set(receivers))
-    return R, sets
+        tmp_set = R if len(R) < m and all_is_disjoint(R, receivers) else R_other
+        tmp_set.append(set(receivers))
+    return R, R_other
 
-def excluding_phase(R, sets):
-    for item_set in sets:
+def excluding_phase(R, R_other):
+    for item_set in R_other:
         disjoint_sets = [i for i in range(len(R)) if not(R[i].isdisjoint(item_set))]
         if len(disjoint_sets) == 1:
             R[disjoint_sets[0]] &= item_set
     return R
 
 def find_partners(sender_ip, mix_ip, m, file_in):
-    R, sets = learning_phase(sender_ip, mix_ip, m, file_in)
-    partners = excluding_phase(R, sets)
+    R, R_other = learning_phase(sender_ip, mix_ip, m, file_in)
+    partners = excluding_phase(R, R_other)
     ip_sum = get_ip_sum(partners)
     return ip_sum
 
 def get_ip_sum(R):
-    ip_ints = [ip_to_int("".join(item_set)) for item_set in R]
+    ip_ints = [int(IPv4Address(next(iter(item_set)))) for item_set in R]
     return sum(ip_ints)
 
-def ip_to_int(ip):
-    hex_str = [int_to_hex(int(ip))[2:].zfill(2) for ip in ip.split(".")]
-    string = "0x" + "".join(hex_str)
-    return hex_to_int(string)
-
-def is_disjoint(list_of_sets, set2):
-    joint_sets = [0 for item_set in list_of_sets if not(item_set.isdisjoint(set2))]
-    return len(joint_sets) == 0
 
 def parse_pcap_file(file_in):
     testcap = open(file_in, 'rb')
@@ -52,5 +48,4 @@ def parse_pcap_file(file_in):
 
 if __name__ == '__main__':
     print(find_partners('159.237.13.37', '94.147.150.188', 2, 'inputs/B2_cia.log.1337.pcap'))
-    # print(find_partners("161.53.13.37", "11.192.206.171", 12, 'inputs/B2_cia.log.1339.pcap'))
-    # print(find_partners('245.221.13.37','15.24.22.93',9, 'inputs/cia.log.2.pcap'))
+    print(find_partners("161.53.13.37", "11.192.206.171", 12, 'inputs/B2_cia.log.1339.pcap'))
