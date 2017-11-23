4c28d45352425a255bab74cf6316bd5e2af3ebcc
antongoransson <anton.goransson95@gmail.com>
2017-11-23 09:47:15 +0100

    changed variable names

--- HA2/B1.py
+++ HA2/B1.py
@@ -1,15 +1,14 @@
 from converter import *
 import binascii
 
-def dc_broadcast(secretA, secretB, dataA, dataB, message, b):
-    if(b):
-        myBroadcast = hex_to_int(dataA) ^ hex_to_int(dataB) ^ hex_to_int(message)
-        myBroadcast = int_to_hex(myBroadcast)[2:].zfill(4)
+def dc_broadcast(SA, SB, DA, DB, m, b):
+    if(b == 1):
+        BC = int(DA, 16) ^ int(DB, 16) ^ int(m, 16)
+        BC = hex(BC)[2:].zfill(4)
     else:
-        myData = hex_to_int(secretA) ^ hex_to_int(secretB)
-        myBroadcast = hex_to_int(dataA) ^ hex_to_int(dataB) ^ myData
-        myBroadcast = int_to_hex((myData))[2:] + int_to_hex(myBroadcast)[2:].zfill(4)
-    return myBroadcast.upper()
+        D = int(SA, 16) ^ int(SB, 16)
+        BC = hex(D)[2:] + hex(int(DA, 16) ^ int(DB, 16) ^ D)[2:].zfill(4)
+    return BC.upper()
 
 if __name__ == '__main__':
     SA = 'BF0D'
