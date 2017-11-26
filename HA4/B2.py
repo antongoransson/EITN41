1c72db3e7bf37e8e34fa63fe43ca0211822e8bb2
antongoransson <anton.goransson95@gmail.com>
2017-11-26 22:19:02 +0100

    Added loop for if something went wrong during calculation

--- HA4/B2.py
+++ HA4/B2.py
@@ -1,23 +1,30 @@
 import requests
 requests.packages.urllib3.disable_warnings()
 
-# https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Kalle&grade=5&signature=6823...ba36
+# https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Kalle&grade=5&signature=6823
 def brute_force(name, grade, string="0123456789abcdef", length=20):
     sign = "" #6823ea50b133c58cba36
     url = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?"
-    payload = {'name': name, 'grade': grade, 'signature': sign}
-    while len(sign) < length:
-        req_times = {}
-        for char in string:
-            try_sign = sign + char
-            payload['signature'] = try_sign
-            req = requests.get(url, params=payload, verify=False)
-            req_times[try_sign] = req.elapsed.total_seconds()
-        sign = max(req_times, key = req_times.get)
-    payload['signature'] = sign
-    r = requests.get(url, params=payload, verify=False)
-    print(len(r.text))
+    payload = {"name": name, "grade": grade, "signature": sign}
+    ver = False
+    while not(ver):
+        while len(sign) < length:
+            req_times = {}
+            for char in string:
+                try_sign = sign + char
+                payload["signature"] = try_sign
+                req = requests.get(url, params=payload, verify=False)
+                req_times[try_sign] = req.elapsed.total_seconds()
+            sign = max(req_times, key = req_times.get)
+        payload["signature"] = sign
+        r = requests.get(url, params=payload, verify=False)
+        ver = r.text.strip() == "1"
+    print("Request: ?name={}&grade={}&signature={}".format(name, grade, sign))
     print("Signature valid: {}".format(r.text.strip() == "1"))
     print("Name: {}, Grade: {}, Signature: {}".format(name, grade, sign))
+    return sign
 
-brute_force("Jonathan", 3)
+if __name__ == '__main__':
+    sign=""
+    brute_force("Jonathan", 3)
+    brute_force("Kalle", 5)
