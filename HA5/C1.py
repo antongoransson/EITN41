6d3d184fd76e42955c65369d9fd7495fc4144498
antongoransson <anton.goransson95@gmail.com>
2017-12-08 18:16:24 +0100

    Moved parameters to file

--- HA5/C1.py
+++ HA5/C1.py
@@ -2,9 +2,6 @@ from hashlib import sha1
 from random import randrange
 from sys import argv
 
-def h_int(h):
-    return int(h, 16)
-
 def b_int(b):
     return int.from_bytes(b, byteorder='big')
 
@@ -29,51 +26,50 @@ def extendex_euc_alg(x, n):
         q, x, n = x // n, n, x % n
         x0, x1 = x1, x0 - q * x1
         y0, y1 = y1, y0 - q * y1
-    return  x, x0
+    return  x, x0, y0
 
 # x = mulinv(b) mod n, (x * b) % n == 1
 def mulinv(b, n):
-    g, x = extendex_euc_alg(b, n)
+    g, x, _ = extendex_euc_alg(b, n)
     if g == 1:
         return x % n
 
 def PKG(a, m, p, q):
     return pow(a, (m + 5 - p - q) // 8, m)
 
-def find_a(identity, m):
-    h = sha1(identity.encode('utf8'))
-    while jacobi(b_int(h.digest()), m) != 1:
-        h = sha1(h.digest())
-    return h.hexdigest()
+def quadratic_residue(identity, m):
+    a = sha1(identity.encode('utf8')).digest()
+    while jacobi(b_int(a), m) != 1:
+        a = sha1(a).digest()
+    return b_int(a)
 
-def find_t(m):
+def get_t(m):
     t = randrange(0, m)
     while jacobi(t, m) != 1:
         t = randrange(0, m)
     return t
 
-def find_bit(encrypted_bits, r, m):
-    b = [jacobi(h_int(s) + 2 * r, m) for s in encrypted_bits]
+def decrypt_bits(encrypted_bits, r, m):
+    b = [jacobi(int(s, 16) + 2 * r, m) for s in encrypted_bits]
     b = [0 if x < 0 else x for x in b]
     return int(''.join(map(str, b)), 2)
 
-def IBE(identity, p , q):
+def IBE(identity, p , q, encrypted_bits):
     m = p  * q
-    encrypted_bits = []
-    while True:
-        try: encrypted_bits.append((input('')))
-        except: break
-    a = h_int(find_a(identity, m))
+    a = quadratic_residue(identity, m)
     r = PKG(a, m, p, q)
-    t = find_t(m)
+    t = get_t(m)
     s = (t + a * mulinv(t, m)) % m
-    code = find_bit(encrypted_bits, r, m)
-    print('r:', hex(r)[2:])
+    code = decrypt_bits(encrypted_bits, r, m)
+    print('r:', format(r, 'x'))
     print('CODE:', code)
 
 
 if __name__ == '__main__':
-    identity = 'walterwhite@crypto.sec'
-    p = h_int('9240633d434a8b71a013b5b00513323f')
-    q = h_int('f870cfcd47e6d5a0598fc1eb7e999d1b')
-    IBE(identity, p, q)
+    identity = input('')
+    p, q = int(input(''), 16), int(input(''), 16)
+    encrypted_bits = []
+    while True:
+        try: encrypted_bits.append((input('')))
+        except: break
+    IBE(identity, p, q, encrypted_bits)
